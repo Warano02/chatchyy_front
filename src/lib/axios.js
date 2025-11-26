d906437cf7b01c6775ede2dd62b338c441aeb7f2
Warano02 <carineteoi@gmail.com>
2025-11-26 11:22:41 +0100

    ..

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chatchyy-back.onrender.com/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chatchyy-back-majy.onrender.com/api",
   withCredentials: true,
 });
